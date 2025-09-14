57929998f58ff81cdb9fa2e1e3a9c73013c4b76f
Kathryn Le <kathrynlle9@gmail.com>
2025-09-14 01:07:53 -0400

    adding experiences

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,6 @@ var feedApiRouter = require('./routes/feed-api');
 var profileRouter = require('./routes/profile');
 var feedApiRouter = require('./routes/feed-api');
 var profileRouter = require('./routes/profile');
-var feedApiRouter = require('./routes/feed-api');
 
 // Import passport configuration
 require('./config/passport');
@@ -62,7 +61,6 @@ app.use('/feed-api', feedApiRouter);
 app.use('/profile', profileRouter);
 app.use('/feed-api', feedApiRouter);
 app.use('/profile', profileRouter);
-app.use('/feed-api', feedApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
